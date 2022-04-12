163ac5f980dd7bd4160008f9872697d0fe183fd8
Krzysztof Purgat <kr.purgat@gmail.com>
2022-04-12 17:00:51 +0200

    Add calculating total

--- Food_Order/app.js
+++ Food_Order/app.js
@@ -3,94 +3,155 @@ const menuItems = [
     name: "French Fries with Ketchup",
     price: 2.23,
     image: "images/plate__french-fries.png",
-    alt: "FrenchFries",
+    alt: "French Fries",
+    id: "FrenchFries",
     count: 0,
   },
   {
     name: "Salmon and Vegetables",
     price: 5.12,
     image: "images/plate__salmon-vegetables.png",
-    alt: "SalmonandVegetables",
+    alt: "Salmonand Vegetables",
+    id: "SalmonandVegetables",
     count: 0,
   },
   {
     name: "Spaghetti Meat Sauce",
     price: 7.82,
     image: "images/plate__spaghetti-meat-sauce.png",
-    alt: "SpaghettiwithMeatSauce",
+    alt: "Spaghetti with Meat Sauce",
+    id: "SpaghettiwithMeatSauce",
     count: 0,
   },
   {
     name: "Bacon, Eggs, and Toast",
     price: 5.99,
     image: "images/plate__bacon-eggs.png",
-    alt: "BaconEggsandToast",
+    alt: "Bacon, Eggs and Toast",
+    id: "BaconEggsandToast",
     count: 0,
   },
   {
     name: "Chicken Salad with Parmesan",
     price: 6.98,
     image: "images/plate__chicken-salad.png",
-    alt: "ChickeSaladwithParmesan",
+    alt: "Chicke Saladwith Parmesan",
+    id: "ChickeSaladwithParmesan",
     count: 0,
   },
   {
     name: "Fish Sticks and Fries",
     price: 6.34,
     image: "images/plate__fish-sticks-fries.png",
-    alt: "FishSticksandFries",
+    alt: "Fish Sticks and Fries",
+    id: "FishSticksandFries",
     count: 0,
   },
 ];
 
-const add = document.getElementsByClassName("add");
+const addToCartButtons = document.getElementsByClassName("add");
 const cartSummary = document.getElementsByClassName("cart-summary")[0];
 const empty = document.getElementsByClassName("empty")[0];
 
-let plateId = 0;
+const removeItem = (selectedItemIndex) => {
+  addToCartButtons[selectedItemIndex].classList.remove("in-cart");
+  addToCartButtons[selectedItemIndex].innerHTML = `Add to Cart`;
 
-const changeState = (counter) => () => {
-  if (add[counter].innerHTML === "Add to Cart") {
-    add[counter].classList.add("in-cart");
-    add[
-      counter
+  menuItems[selectedItemIndex].count = 0;
+  const item = document.getElementById(
+    `cart_${menuItems[selectedItemIndex].id}`
+  );
+  item.parentNode.removeChild(item);
+  if (cartSummary.innerHTML === "")
+    empty.removeAttribute("style", "display: none;");
+};
+
+const showQuantity = (selectedItemIndex) => {
+  const quantities = document.querySelectorAll(
+    `#cart_${menuItems[selectedItemIndex].id} .quantity`
+  );
+  quantities.forEach((element) => {
+    element.innerHTML = menuItems[selectedItemIndex].count;
+  });
+};
+
+const calculateTotal = () => {
+  const prices = document.querySelectorAll(`.cart-summary .price`);
+  const quantities = document.querySelectorAll(
+    ".cart-summary .quantity-display"
+  );
+  const subtotal = document.getElementsByClassName("amount price subtotal")[0];
+  const tax = document.getElementsByClassName("amount price tax")[0];
+  const total = document.getElementsByClassName("amount price total")[0];
+
+  let subtotalValue = 0;
+
+  prices.forEach((price, index) => {
+    subtotalValue += +price.innerText.substr(1) * +quantities[index].innerText;
+  });
+  subtotalValue = subtotalValue.toFixed(2);
+  subtotal.innerHTML = `$${subtotalValue}`;
+
+  let taxValue = (+subtotalValue * 0.0975).toFixed(2);
+  tax.innerHTML = `$${taxValue}`;
+
+  let totalValue = (+subtotalValue + +taxValue).toFixed(2);
+  total.innerHTML = `$${totalValue}`;
+};
+
+const changeState = (selectedItemIndex) => () => {
+  if (addToCartButtons[selectedItemIndex].innerHTML === "Add to Cart") {
+    addToCartButtons[selectedItemIndex].classList.add("in-cart");
+    addToCartButtons[
+      selectedItemIndex
     ].innerHTML = `<img src="images/check.svg" alt="Check" /> In Cart`;
 
     empty.setAttribute("style", "display: none;");
-    menuItems[counter].count = 1;
-    cartSummary.innerHTML += `<li id="${menuItems[counter].alt}"><div class="plate">
-    <img src=${menuItems[counter].image} alt=${menuItems[counter].alt}class="plate" />
-    <div class="quantity">1</div>
+    menuItems[selectedItemIndex].count = 1;
+    cartSummary.innerHTML += `<li id="cart_${menuItems[selectedItemIndex].id}"><div class="plate">
+    <img src=${menuItems[selectedItemIndex].image} alt=${menuItems[selectedItemIndex].alt}class="plate" />
+    <div class="quantity quantity-display">1</div>
   </div>
   <div class="content">
-    <p class="menu-item">${menuItems[counter].name}</p>
-    <p class="price">$${menuItems[counter].price}</p>
+    <p class="menu-item">${menuItems[selectedItemIndex].name}</p>
+    <p class="price">$${menuItems[selectedItemIndex].price}</p>
   </div>
   <div class="quantity__wrapper">
-    <button class="decrease">
+    <button class="decrease" onclick="decreaseCount(${selectedItemIndex})">
       <img src="images/chevron.svg" />
     </button>
-    <div class="quantity">${menuItems[counter].count}</div>
-    <button class="increase">
+    <div class="quantity">${menuItems[selectedItemIndex].count}</div>
+    <button class="increase" onclick="increaseCount(${selectedItemIndex})">
       <img src="images/chevron.svg" />
     </button>
   </div>
   <div class="subtotal">
-    $${menuItems[counter].price}
+    $${menuItems[selectedItemIndex].price}
   </div></li>`;
-    plateId++;
+    calculateTotal();
   } else {
-    add[counter].classList.remove("in-cart");
-    add[counter].innerHTML = `Add to Cart`;
-
-    menuItems[counter].count = 0;
-    const item = document.getElementById(`${menuItems[counter].alt}`);
-    item.parentNode.removeChild(item);
-    if (cartSummary.innerHTML === "")
-      empty.removeAttribute("style", "display: none;");
+    removeItem(selectedItemIndex);
+    calculateTotal();
   }
 };
 
-for (let counter = 0; counter < add.length; counter++) {
-  add[counter].addEventListener("click", changeState(counter));
+const increaseCount = (selectedItemIndex) => {
+  menuItems[selectedItemIndex].count++;
+  showQuantity(selectedItemIndex);
+  calculateTotal();
+};
+
+const decreaseCount = (selectedItemIndex) => {
+  if (menuItems[selectedItemIndex].count === 1) {
+    removeItem(selectedItemIndex);
+    calculateTotal();
+  } else {
+    menuItems[selectedItemIndex].count--;
+    showQuantity(selectedItemIndex);
+    calculateTotal();
+  }
+};
+
+for (let counter = 0; counter < addToCartButtons.length; counter++) {
+  addToCartButtons[counter].addEventListener("click", changeState(counter));
 }
