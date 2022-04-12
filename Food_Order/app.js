fd464a6c88902e654037d8802777b2bc70d460e4
Krzysztof Purgat <kr.purgat@gmail.com>
2022-04-12 12:43:26 +0200

    Add removing from the cart

--- Food_Order/app.js
+++ Food_Order/app.js
@@ -3,48 +3,51 @@ const menuItems = [
     name: "French Fries with Ketchup",
     price: 2.23,
     image: "images/plate__french-fries.png",
-    alt: "French Fries",
+    alt: "FrenchFries",
     count: 0,
   },
   {
     name: "Salmon and Vegetables",
     price: 5.12,
     image: "images/plate__salmon-vegetables.png",
-    alt: "Salmon and Vegetables",
+    alt: "SalmonandVegetables",
     count: 0,
   },
   {
     name: "Spaghetti Meat Sauce",
     price: 7.82,
     image: "images/plate__spaghetti-meat-sauce.png",
-    alt: "Spaghetti with Meat Sauce",
+    alt: "SpaghettiwithMeatSauce",
     count: 0,
   },
   {
     name: "Bacon, Eggs, and Toast",
     price: 5.99,
     image: "images/plate__bacon-eggs.png",
-    alt: "Bacon, Eggs, and Toast",
+    alt: "BaconEggsandToast",
     count: 0,
   },
   {
     name: "Chicken Salad with Parmesan",
     price: 6.98,
     image: "images/plate__chicken-salad.png",
-    alt: "Chicken Salad with Parmesan",
+    alt: "ChickeSaladwithParmesan",
     count: 0,
   },
   {
     name: "Fish Sticks and Fries",
     price: 6.34,
     image: "images/plate__fish-sticks-fries.png",
-    alt: "Fish Sticks and Fries",
+    alt: "FishSticksandFries",
     count: 0,
   },
 ];
+
 const add = document.getElementsByClassName("add");
 const cartSummary = document.getElementsByClassName("cart-summary")[0];
 const empty = document.getElementsByClassName("empty")[0];
+
+let plateId = 0;
 
 const changeState = (counter) => () => {
   if (add[counter].innerHTML === "Add to Cart") {
@@ -55,31 +58,36 @@ const changeState = (counter) => () => {
 
     empty.setAttribute("style", "display: none;");
     menuItems[counter].count = 1;
-    cartSummary.innerHTML += `<li>
-    <div class="plate">
-      <img src=${menuItems[counter].image} alt=${menuItems[counter].alt}class="plate" />
-      <div class="quantity">1</div>
-    </div>
-    <div class="content">
-      <p class="menu-item">${menuItems[counter].name}</p>
-      <p class="price">$${menuItems[counter].price}</p>
-    </div>
-    <div class="quantity__wrapper">
-      <button class="decrease">
-        <img src="images/chevron.svg" />
-      </button>
-      <div class="quantity">${menuItems[counter].count}</div>
-      <button class="increase">
-        <img src="images/chevron.svg" />
-      </button>
-    </div>
-    <div class="subtotal">
-      $${menuItems[counter].price}
-    </div>
-  </li>`;
+    cartSummary.innerHTML += `<li id="${menuItems[counter].alt}"><div class="plate">
+    <img src=${menuItems[counter].image} alt=${menuItems[counter].alt}class="plate" />
+    <div class="quantity">1</div>
+  </div>
+  <div class="content">
+    <p class="menu-item">${menuItems[counter].name}</p>
+    <p class="price">$${menuItems[counter].price}</p>
+  </div>
+  <div class="quantity__wrapper">
+    <button class="decrease">
+      <img src="images/chevron.svg" />
+    </button>
+    <div class="quantity">${menuItems[counter].count}</div>
+    <button class="increase">
+      <img src="images/chevron.svg" />
+    </button>
+  </div>
+  <div class="subtotal">
+    $${menuItems[counter].price}
+  </div></li>`;
+    plateId++;
   } else {
     add[counter].classList.remove("in-cart");
     add[counter].innerHTML = `Add to Cart`;
+
+    menuItems[counter].count = 0;
+    const item = document.getElementById(`${menuItems[counter].alt}`);
+    item.parentNode.removeChild(item);
+    if (cartSummary.innerHTML === "")
+      empty.removeAttribute("style", "display: none;");
   }
 };
 
