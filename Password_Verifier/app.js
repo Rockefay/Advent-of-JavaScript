36cacd28fd47e0fe8a1ff92f7dbdafcdf7088e96
Krzysztof Purgat <kr.purgat@gmail.com>
2022-07-27 14:32:21 +0200

    Allow pasting the code to separate positions

--- Password_Verifier/app.js
+++ Password_Verifier/app.js
@@ -7,3 +7,21 @@ function nextNumber(place, nextPlace) {
   if (place.value.length == place.maxLength)
     document.getElementById(nextPlace).focus();
 }
+
+//Pasting the code
+for (let i = 0; i < verification.length; i++) {
+  verification[i].addEventListener("paste", (e) => {
+    var clipboardData, pastedData;
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData("Text");
+
+    for (let index = i, j = 0; index < verification.length; index++, j++) {
+      verification[index].value = pastedData[j];
+    }
+    verification[verification.length - 1].focus();
+  });
+}
