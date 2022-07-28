0daca9825724b28bc7351e27cc3ba3b726b892f0
Krzysztof Purgat <kr.purgat@gmail.com>
2022-07-28 14:19:38 +0200

    Chane svg after expanding

--- Collapsable/app.js
+++ Collapsable/app.js
@@ -1,12 +1,15 @@
 const collapsable = document.getElementsByClassName("collapsable");
 const expand = document.getElementsByClassName("expand")[0];
+const question = document.getElementsByClassName("question");
 
 for (let i = 0; i < collapsable.length; i++) {
   collapsable[i].addEventListener("click", () => {
     if (!collapsable[i].classList.contains("expand")) {
       collapsable[i].classList.add("expand");
+      question[i].classList.add("question-expanded");
     } else {
       collapsable[i].classList.remove("expand");
+      question[i].classList.remove("question-expanded");
     }
   });
 }
