802f0dc63990162cd1c7191a7e0453b3222c2ad2
Krzysztof Purgat <kr.purgat@gmail.com>
2022-07-29 15:00:40 +0200

    Decide winner or show draw

--- Rock_Paper_Scissors/winner.js
+++ Rock_Paper_Scissors/winner.js
@@ -1,27 +1,56 @@
 const playAgain = document.getElementsByClassName("play-again")[0];
 const yourImg = document.getElementsByClassName("your-img")[0];
 const computerImg = document.getElementsByClassName("computer-img")[0];
+const winner = document.getElementsByClassName("winner")[0];
 
 playAgain.addEventListener("click", () => {
   window.location = "./index.html";
 });
 
-/*
-Choices:
-0 - rock
-1 - paper
-2 - scissors
- */
+const choices = {
+  Rock: { weakTo: "Paper", strongTo: "Scissors" },
+  Paper: { weakTo: "Scissors", strongTo: "Rock" },
+  Scissors: { weakTo: "Rock", strongTo: "Paper" },
+};
 
-const choice = localStorage.getItem("choice");
-const computerChoice = Math.floor(Math.random() * (2 - 0)) + 0;
-console.log(yourImg);
+let choice = localStorage.getItem("choice");
+let computerChoice = Math.floor(Math.random() * (4 - 1)) + 1;
 
-if (choice == 0) yourImg.setAttribute("src", "./images/rock.png");
-else if (choice == 1) yourImg.setAttribute("src", "./images/paper.png");
-else yourImg.setAttribute("src", "./images/scissors.png");
+if (choice == 0) {
+  yourImg.setAttribute("src", "./images/rock.png");
+  choice = "Rock";
+} else if (choice == 1) {
+  yourImg.setAttribute("src", "./images/paper.png");
+  choice = "Paper";
+} else {
+  yourImg.setAttribute("src", "./images/scissors.png");
+  choice = "Scissors";
+}
 
-if (computerChoice == 0) computerImg.setAttribute("src", "./images/rock.png");
-else if (computerChoice == 1)
+if (computerChoice == 1) {
+  computerImg.setAttribute("src", "./images/rock.png");
+  computerChoice = "Rock";
+} else if (computerChoice == 2) {
   computerImg.setAttribute("src", "./images/paper.png");
-else computerImg.setAttribute("src", "./images/scissors.png");
+  computerChoice = "Paper";
+} else {
+  computerImg.setAttribute("src", "./images/scissors.png");
+  computerChoice = "Scissors";
+}
+
+function decideWinner(player, computer) {
+  if (choices[player].strongTo === computer) playerWins();
+  if (choices[player].weakTo === computer) computerWins();
+  if (player === computer) draw();
+}
+function computerWins() {
+  winner.classList.add("computer-wins");
+}
+function playerWins() {
+  winner.classList.add("you-win");
+}
+function draw() {
+  winner.classList.add("draw");
+}
+
+decideWinner(choice, computerChoice);
