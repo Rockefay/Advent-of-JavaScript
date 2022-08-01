24603539f0a89d56d14b7258540f4506c60d75f0
Krzysztof Purgat <kr.purgat@gmail.com>
2022-08-01 10:48:58 +0200

    Change featured video after choosing from side panel

--- YouTube_Video_Gallery/app.js
+++ YouTube_Video_Gallery/app.js
@@ -3,20 +3,34 @@ import { sampleAPIResponse } from "./sampleData.js";
 const video = document.getElementsByClassName("video");
 const thumbnail = document.getElementsByClassName("thumbnail");
 const title = document.getElementsByClassName("title");
+const featureVideo = document.getElementsByClassName("feature-video")[0];
 const featureTitle = document.getElementsByClassName("feature-title")[0];
 const featureDescription = document.getElementsByClassName(
   "feature-description"
 )[0];
 
-//set title and description of feature video
-featureTitle.innerHTML = sampleAPIResponse.items[0].snippet.title;
-featureDescription.innerHTML = sampleAPIResponse.items[0].snippet.description;
+setFeatureVideo(0);
 
-//setting videos on side panel
 for (let i = 0; i < video.length; i++) {
+  //setting videos on side panel
   thumbnail[i].setAttribute(
     "src",
     sampleAPIResponse.items[i].snippet.thumbnails.default.url
   );
   title[i].innerHTML = sampleAPIResponse.items[i].snippet.title;
+
+  video[i].addEventListener("click", () => {
+    setFeatureVideo(i);
+  });
+}
+
+function setFeatureVideo(index) {
+  const YTUrl = "https://www.youtube.com/embed/";
+  featureVideo.setAttribute(
+    "src",
+    YTUrl + sampleAPIResponse.items[index].id.videoId
+  );
+  featureTitle.innerHTML = sampleAPIResponse.items[index].snippet.title;
+  featureDescription.innerHTML =
+    sampleAPIResponse.items[index].snippet.description;
 }
