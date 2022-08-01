8c3ca40a33c14a45bd0ec4c186939ff2baa41f2a
Krzysztof Purgat <kr.purgat@gmail.com>
2022-08-01 10:33:57 +0200

    Set title and description of featured video

--- YouTube_Video_Gallery/app.js
+++ YouTube_Video_Gallery/app.js
@@ -3,11 +3,20 @@ import { sampleAPIResponse } from "./sampleData.js";
 const video = document.getElementsByClassName("video");
 const thumbnail = document.getElementsByClassName("thumbnail");
 const title = document.getElementsByClassName("title");
+const featureTitle = document.getElementsByClassName("feature-title")[0];
+const featureDescription = document.getElementsByClassName(
+  "feature-description"
+)[0];
 
+//set title and description of feature video
+featureTitle.innerHTML = sampleAPIResponse.items[0].snippet.title;
+featureDescription.innerHTML = sampleAPIResponse.items[0].snippet.description;
+
+//setting videos on side panel
 for (let i = 0; i < video.length; i++) {
   thumbnail[i].setAttribute(
     "src",
-    sampleAPIResponse.items[i + 1].snippet.thumbnails.default.url
+    sampleAPIResponse.items[i].snippet.thumbnails.default.url
   );
-  title[i].innerHTML = sampleAPIResponse.items[i + 1].snippet.title;
+  title[i].innerHTML = sampleAPIResponse.items[i].snippet.title;
 }
