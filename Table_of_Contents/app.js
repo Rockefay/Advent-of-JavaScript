5df17b8a651be2d580320c415038d94f9be1eebf
Krzysztof Purgat <kr.purgat@gmail.com>
2022-08-02 12:50:09 +0200

    Fix showing one topic

--- Table_of_Contents/app.js
+++ Table_of_Contents/app.js
@@ -7,10 +7,18 @@ document.addEventListener("scroll", () => {
     else document.querySelector(`li#${id}`).classList.remove("selected");
   }
   if (document.getElementsByClassName("selected").length > 1)
-    document.getElementsByClassName("selected")[0].classList.remove("selected");
+    for (
+      let i = 0;
+      i < document.getElementsByClassName("selected").length;
+      i++
+    ) {
+      document
+        .getElementsByClassName("selected")
+        [i].classList.remove("selected");
+    }
 });
 function isInView(el) {
   let data = el.getBoundingClientRect();
-  if (data.top < 300 && data.top > 0) return true;
+  if (data.top < 800 && data.top > 0) return true;
   else return false;
 }
