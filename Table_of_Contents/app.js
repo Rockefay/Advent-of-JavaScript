0c54e158672ff8b5605907e516ce566d4804384b
Krzysztof Purgat <kr.purgat@gmail.com>
2022-08-02 12:08:55 +0200

    Show only one current topic in table of contents

--- Table_of_Contents/app.js
+++ Table_of_Contents/app.js
@@ -1,12 +1,16 @@
-var observer = new IntersectionObserver(function (entries) {
-  for (let i = 0; i < entries.length; i++) {
-    const id = entries[i].target.getAttribute("class");
-    if (entries[i].isIntersecting === true) {
-      document.querySelector(`li#${id}`).classList.add("selected");
-    } else document.querySelector(`li#${id}`).classList.remove("selected");
-  }
-});
 const items = document.querySelectorAll("h3");
-for (let i = 0; i < items.length; i++) {
-  observer.observe(items[i]);
+document.addEventListener("scroll", () => {
+  for (let i = 0; i < items.length; i++) {
+    const id = items[i].getAttribute("class");
+    if (isInView(items[i]))
+      document.querySelector(`li#${id}`).classList.add("selected");
+    else document.querySelector(`li#${id}`).classList.remove("selected");
+  }
+  if (document.getElementsByClassName("selected").length > 1)
+    document.getElementsByClassName("selected")[0].classList.remove("selected");
+});
+function isInView(el) {
+  let data = el.getBoundingClientRect();
+  if (data.top < 300 && data.top > 0) return true;
+  else return false;
 }
