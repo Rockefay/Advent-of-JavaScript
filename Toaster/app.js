6ffe7b0977e06e0328a299be5a870da2046bf901
Krzysztof Purgat <kr.purgat@gmail.com>
2022-08-09 11:24:04 +0200

    Display the popup after 15 seconds

--- Toaster/app.js
+++ Toaster/app.js
@@ -2,13 +2,30 @@ const toaster = document.getElementsByClassName("toaster")[0];
 const closeButton = document.getElementById("closeToaster");
 
 toaster.classList.add("collapsed");
+let myTimeout = setTimeout(showPopup, 15000);
 
 document.addEventListener("mousemove", function (event) {
-  if (event.clientY < 50) {
-    toaster.classList.remove("collapsed");
-  }
+  if (event.clientY < 150) showPopup();
 });
 
 closeButton.addEventListener("click", () => {
-  toaster.classList.add("collapsed");
+  hidePopup();
+  window.clearTimeout(myTimeout);
+  myTimeout = setTimeout(showPopup, 15000);
 });
+
+document.addEventListener("visibilitychange", function () {
+  if (document.hidden) {
+    window.clearTimeout(myTimeout);
+  } else {
+    myTimeout = setTimeout(showPopup, 15000);
+  }
+});
+
+function showPopup() {
+  toaster.classList.remove("collapsed");
+}
+
+function hidePopup() {
+  toaster.classList.add("collapsed");
+}
